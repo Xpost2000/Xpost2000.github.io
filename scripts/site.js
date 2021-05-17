8ba88712426b99ce2fb2483434052339e8acac24
Xpost2000 <xpost2000cod@gmail.com>
2021-05-17 19:54:27 -0400

    theme selector made

--- scripts/site.js
+++ scripts/site.js
@@ -1,5 +1,6 @@
 // It was either this or populate the html file itself.
 // Might as well generate it after it's loaded.
+const avaliable_themes = ['default', 'light'];
 
 // I'm sure there's a better way, but I can always fix this later.
 function paste_lots_of_tildes(event) {
@@ -22,7 +23,7 @@ function handle_resize(event) {
     gutter.style.height = window.innerHeight * 1.1;
 }
 
-let theme = function() {
+let current_theme = function() {
     let theme = localStorage.getItem('theme');
     // I wish I was joking
     if (theme === null || theme === undefined || theme === "" || theme === "null") {
@@ -44,13 +45,12 @@ function update_theme() {
     console.log("updating theme");
 
     let relative_path_prefixes = ["styles/", "../styles/", "../../styles/", "../../../styles/"];
-    console.log(theme);
     for (const prefix of relative_path_prefixes) {
-        document.head.appendChild(stylesheet_link(`${prefix}${theme}.css`));
+        document.head.appendChild(stylesheet_link(`${prefix}${current_theme}.css`));
     }
 }
 function switch_to_theme(new_theme) {
-    theme = new_theme;
+    current_theme = new_theme;
     localStorage.setItem('theme', new_theme);
 
     update_theme();
@@ -75,11 +75,78 @@ mini_buffer.addEventListener("click", function(event) {
 mini_buffer_autocompletion.addEventListener("mouseout", function(event) { mini_buffer_autocompletion.style.display = "none"; });
 mini_buffer_autocompletion.addEventListener("click", function(event) { mini_buffer_autocompletion.style.display = "none"; });
 
+let theme_selector_visible = false;
+let theme_selector = function() {
+    let holder_div = document.createElement("div");
+    holder_div.id = "theme-selector";
+
+    let box_holder_div = document.createElement("div");
+    box_holder_div.id = "theme-selector-box";
+
+    holder_div.appendChild(box_holder_div);
+
+    {
+        let header = document.createElement("h1");
+        header.appendChild(document.createTextNode("Theme Selection"));
+        let paragraph = document.createElement("p");
+        paragraph.appendChild(document.createTextNode(`
+          Use this little selector widget to change the current theme for the site. It doesn't really
+          serve any practical usage, but it's cool to see things change. I may add more themes whenever
+          I feel like it. Enjoy the selection.
+        `));
+        box_holder_div.appendChild(header);
+        box_holder_div.appendChild(paragraph);
+        box_holder_div.appendChild(document.createElement("br"));
+
+        let selector = document.createElement("select");
+        selector.id="theme-selection-selector";
+
+        for (const theme of avaliable_themes) {
+            let option = document.createElement("option");
+            option.setAttribute("value", theme);
+            if (theme === current_theme) {
+                option.setAttribute("selected", "true");
+            }
+            option.appendChild(document.createTextNode(theme));
+            selector.appendChild(option);
+        }
+
+        selector.addEventListener('change',
+                                  function(event) {
+                                      switch_to_theme(event.target.value);
+                                  });
+
+        box_holder_div.appendChild(selector);
+
+        let button = document.createElement("button");
+        button.appendChild(document.createTextNode("Close Selector"));
+        button.id = "theme-selection-close-button";
+        button.addEventListener("click",
+                                function() {
+                                    close_theme_selector();
+                                });
+        box_holder_div.appendChild(button);
+    }
+
+    return holder_div;
+}();
+function open_theme_selector() {
+    document.body.appendChild(theme_selector);
+    theme_selector_visible = true;
+}
+function close_theme_selector() {
+    document.body.removeChild(theme_selector);
+    theme_selector_visible = false;
+}
+
 document.addEventListener("keydown",
                           function(event) {
                               if (event.key == 'o' && event.ctrlKey) {
-                                  alert("Theme switch demonstration...");
-                                  switch_to_theme(prompt('Enter a theme name', 'default'));
+                                  if (theme_selector_visible) {
+                                      close_theme_selector();
+                                  } else {
+                                      open_theme_selector();
+                                  }
                                   event.preventDefault();
                               }
                           }
